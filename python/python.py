0904c43d65bf15f42c06f364c486a4e05e7a5c15
Aman Kumar <168708176+aman-20232703@users.noreply.github.com>
2025-06-10 23:06:43 +0530

    Update python.py

---
+++ python/python.py
@@ -0,0 +1,26 @@
+""" # calculator
+
+a = int(input("enter 1st number: ", ))
+b = int(input("enter 2nd number: ", ))
+
+print("required results:- ")
+
+#for addition
+adddition=a+b
+print("1.adddition of",a,"and",b,"is",adddition)
+
+#for subtraction
+subtraction=a-b
+print("2.subtraction of",a,"and",b,"is",subtraction)
+
+#for multiplication
+multiplication=a*b
+print("3.multiplication of",a,"and",b,"is",multiplication)
+
+#for division
+division=a/b
+print("4.division of",a,"and",b,"is",division)
+
+print("thankyou :)")
+
+"""
